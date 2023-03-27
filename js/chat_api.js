6737680835cbd7c607a3db09e9ce839f6db2800f
mganeko <masashi.g@gmail.com>
2023-03-27 22:51:27 +0900

    debug log func

--- js/chat_api.js
+++ js/chat_api.js
@@ -2,37 +2,48 @@
 // ChatGPT API wrapper
 //
 
-const chatapi_messages = [{
+// ======= innser variable =========
+const _debugMode = false; // true / false
+const _chatapi_messages = [{
   role: 'system',
   content: 'あなたは親切なアシスタントです',
 }];
 
-async function postText(text, apiKey) {
+// ============== public function ==============
+async function postChatText(text, apiKey) {
   const message = {
     role: 'user',
     content: text,
   };
-  chatapi_messages.push(message);
+  _chatapi_messages.push(message);
 
   // -- compaction --
-  messageCompaction(chatapi_messages);
+  _messageCompaction(_chatapi_messages);
 
-  const response = await chatCompletion(chatapi_messages, apiKey);
-  //console.log(response);
-  chatapi_messages.push(response);
+  const response = await _chatCompletion(_chatapi_messages, apiKey);
+  _debugLog(response);
+  _chatapi_messages.push(response);
 
   return response;
 }
 
+// ============= inner function ============
 
-async function chatCompletion(messages, apiKey) {
+function _debugLog(...args) {
+  if (_debugMode) {
+    console.log(...args);
+  }
+}
+
+async function _chatCompletion(messages, apiKey) {
   //const apiKey = API_KEY;
+  const CHATAPI_URL = "https://api.openai.com/v1/chat/completions";
   const body = JSON.stringify({
     messages,
     model: "gpt-3.5-turbo",
   });
 
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+  const res = await fetch(CHATAPI_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -41,39 +52,39 @@ async function chatCompletion(messages, apiKey) {
     body,
   });
   const data = await res.json();
-  //console.log(data);
-  //console.log(data.usage);
+  //_debugLog(data);
+  _debugLog(data.usage);
 
   const choice = 0;
   return data.choices[choice].message;
 };
 
-function messageCompaction(messages) {
+function _messageCompaction(messages) {
   const TOKEN_LIMIT = 3900;
-  let size = calcTokenSize(messages);
-  //console.log("total token sise:", size);
+  let size = _calcTokenSize(messages);
+  _debugLog("total token sise:", size);
   while (size > TOKEN_LIMIT) {
-    //console.log("Size %d over Limit %d", size, TOKEN_LIMIT);
-    removeMessage(messages);
-    size = calcTokenSize(messages)
+    _debugLog("Size %d over Limit %d", size, TOKEN_LIMIT);
+    _removeMessage(messages);
+    size = _calcTokenSize(messages)
   }
 }
 
-function removeMessage(messages) {
+function _removeMessage(messages) {
   const first = messages.shift();
-  //console.log('remove:', first);
+  _debugLog('remove:', first);
 }
 
 
-function calcTokenSize(messages) {
+function _calcTokenSize(messages) {
   let totalSize = 0;
   messages.forEach(message => {
-    totalSize += calcSingleMessageToken(message);
+    totalSize += _calcSingleMessageToken(message);
   });
   return totalSize;
 }
 
-function calcSingleMessageToken(message) {
+function _calcSingleMessageToken(message) {
   return message.content.length;
 }
 
