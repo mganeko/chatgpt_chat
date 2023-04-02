2daf330370ba95249278ae3f6699935c01afeb89
mganeko <masashi.g@gmail.com>
2023-04-02 21:18:44 +0900

    server error handling

--- js/chat_api.js
+++ js/chat_api.js
@@ -8,6 +8,7 @@ const _debugMode = true; // true / false
 
 const _CHAT_MODEL = "gpt-3.5-turbo";
 const _TOKEN_LIMIT = 3900;
+//const _TOKEN_LIMIT = 100; // for debug, cause ERROR
 
 //const _CHAT_MODEL = "gpt-4";
 //const _TOKEN_LIMIT = 7900;
@@ -63,25 +64,36 @@ async function _chatCompletion(messages, apiKey) {
   }).catch(e => {
     console.error(e);
     return {
-      role: 'assistant',
+      role: 'error',
       content: 'Network ERROR, Plase try again.',
     };
   });
+
+  // エラー判定
+  if (!res.ok) {
+    const responseText = await res.text();
+    _debugLog(res, responseText);
+    return {
+      role: 'error',
+      content: 'Server Error:' + res.status + '. ' + responseText,
+    };
+  }
+
+  // 応答を解析
   const data = await res.json();
   _debugLog(data);
   //_debugLog(data.usage);
-
 
   const choiceIndex = 0;
   //return data.choices[choiceIndex].message;
   const choices = data?.choices;
   if (choices) {
-    return choices[choiceIndex]?.message ?? { role: 'assistant', content : 'Empty'};
+    return choices[choiceIndex]?.message ?? { role: 'error', content : 'Response Empty'};
   }
   else {
     return {
-      role: 'assistant',
-      content: 'Network Erorr, Plase try again.',
+      role: 'error',
+      content: 'Sever Erorr, Plase try again.',
     };
   }
 };
@@ -90,7 +102,7 @@ function _messageCompaction(messages) {
   let size = _calcTokenSize(messages);
   _debugLog("total token sise:", size);
   while (size > _TOKEN_LIMIT) {
-    _debugLog("Message Token total Size %d over Limit %d", size, _TOKEN_LIMIT);
+    _debugLog("Message Token total Size %d, over Limit %d", size, _TOKEN_LIMIT);
     _removeMessage(messages);
     size = _calcTokenSize(messages)
   }
