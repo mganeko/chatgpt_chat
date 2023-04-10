33b5e0b81d45760613d2fea69fca0ce6077c15c9
mganeko <masashi.g@gmail.com>
2023-04-10 22:42:53 +0900

    try streaming API

--- js/chat_api.js
+++ js/chat_api.js
@@ -77,6 +77,55 @@ async function postChatText(text, apiKey) {
   return response;
 }
 
+// ===== streaming function =====
+/*
+ * チャットメッセージを送信し、ストリーミングで応答を返す
+ */
+/**
+* チャットメッセージを送信し、ストリーミングで応答を返す
+* @description _chatapi_messages に配列としてやりとりが蓄積される
+* @param {string} text - ユーザーからのテキスト
+* @param {string} apiKey - OpenAI APIのキー
+* @returns {object} 応答 - { role: 'assistant' / 'error', content: 生成されたテキスト }
+* @example postChatText('世界で一番高い山は？, 'xxxxxxxxxx'); // returns { role: 'assistant', content: 'エベレスト'}
+*/
+async function streamChatText(text, apiKey, chunkHander) {
+  const userMessage = {
+    role: 'user',
+    content: text,
+  };
+
+  // ==== 一時的メッセージ配列を作る ===
+  const tempMessages = Array.from(_chatapi_messages);
+  tempMessages.push(userMessage);
+
+  // -- compaction --
+  _messageCompaction(tempMessages, _TOKEN_LIMIT);
+  _debugLog('after compaction tempMessages:', tempMessages);
+
+  // -- request --
+  const response = await _chatCompletionStream(tempMessages, apiKey, _CHAT_MODEL, chunkHander);
+  _debugLog(response);
+
+  // --- 結果が正常な場合に、userメッセージと合わせて保持する  --
+  // パターン1: 圧縮前のメッセージ配列を保持する場合
+  // if (response.role === 'assistant') {
+  //   _chatapi_messages.push(userMessage);
+  //   _chatapi_messages.push(response);
+  // }
+
+  // パターン2: 圧縮後のメッセージ配列に置き換えて保持する場合
+  if (response.role === 'assistant') {
+    tempMessages.push(response);
+    _chatapi_messages.splice(0, _chatapi_messages.length); // 空にする
+    tempMessages.forEach((m) => _chatapi_messages.push(m)); // 代入する
+  }
+
+  _debugLog('after response, messages:', _chatapi_messages);
+
+  return response;
+}
+
 // ============= helper function ============
 
 // デバッグ用のログ出力
@@ -144,6 +193,125 @@ async function _chatCompletion(messages, apiKey, chatModel) {
     };
   }
 };
+
+// chat API を呼び出し、ストリーミングで応答を返す
+// 参考: https://zenn.dev/himanushi/articles/99579cf407c30b
+async function _chatCompletionStream(messages, apiKey, chatModel, chunkHander) {
+  const CHATAPI_URL = "https://api.openai.com/v1/chat/completions";
+
+  const body = JSON.stringify({
+    messages,
+    model: chatModel,
+    stream: true // ここで stream を有効にする
+  });
+
+  const res = await fetch(CHATAPI_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body,
+  }).catch(e => {
+    console.error(e);
+    return {
+      role: 'error',
+      content: 'Network ERROR, Plase try again.',
+    };
+  });
+
+  // エラー判定
+  if (!res.ok) {
+    const responseText = await res.text();
+    _debugLog(res, responseText);
+    return {
+      role: 'error',
+      content: 'Server Error:' + res.status + '. ' + responseText,
+    };
+  }
+
+  // ReadableStream として使用する
+  const reader = res.body?.getReader();
+  if (!reader) {
+    _debugLog('ERROR to get streaming');
+    return {
+      role: 'error',
+      content: 'Server Error: No Streaming response',
+    };
+  }
+
+  let resultText = '';
+  const decoder = new TextDecoder('utf-8');
+  try {
+    // この read で再起的にメッセージを待機して取得します
+    const read = async () => {
+      const { done, value } = await reader.read();
+      if (done) return reader.releaseLock();
+  
+      const chunk = decoder.decode(value, { stream: true });
+      // この chunk には以下のようなデータ格納されている。複数格納されることもある。
+      // data: { ... }
+      // これは Event stream format と呼ばれる形式
+      // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#event_stream_format
+      //console.log(chunk);
+      
+      const jsons = chunk
+        .split('data:') // 複数格納されていることもあるため split する
+        // data を json parse する
+        // [DONE] は最後の行にくる
+        .map((data) => {
+          const trimData = data.trim();
+          if (trimData === '') {
+            //_debugLog('empty chunk data');
+            return '';
+          }
+          if (trimData === '[DONE]') {
+            //_debugLog('[DONE] chunk data');
+            return '';
+          }
+          return JSON.parse(data.trim());
+        })
+        .filter((data) => data);
+  
+      // あとはこの jsons を好きに使用する
+      //console.log('jsons::', jsons);
+      const text = _buildSteamReulst(jsons);
+      resultText += text;
+      if(chunkHander && typeof chunkHander === 'function') {
+        chunkHander(text);
+      }
+
+      return read();
+    };
+    await read();
+  } catch (e) {
+    console.error(e);
+    return {role: 'error', content: e.message };
+  }
+  finally {
+    // 例外が発生しても、最後は必ず解放する
+    reader.releaseLock();
+  }
+
+  // // ReadableStream を最後は解放する
+  // reader.releaseLock();
+
+  // 最終結果を返す
+  return {role: 'assistant', content: resultText};
+};
+
+function _buildSteamReulst(jsons) {
+  let text = '';
+  for(let i = 0; i < jsons.length; i++) {
+    const json = jsons[i];
+    if (json.choices) {
+      const choice = json.choices[0];
+      const content = choice?.delta?.content ?? '';
+      text += content;
+    }
+  }
+  return text;
+}
 
 // 過去のやり取りを制限に収まるように圧縮する
 function _messageCompaction(messages, tokenLimit) {
