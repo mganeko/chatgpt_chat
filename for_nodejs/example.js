d5e66c28eaa5380c519ba112ef2436aed028c073
mganeko <masashi.g@gmail.com>
2023-04-30 20:35:58 +0900

    remove needless await

--- for_nodejs/example.js
+++ for_nodejs/example.js
@@ -12,7 +12,7 @@ const API_KEY=process.env.OPENAI_API_KEY;
 const chatapi = require('./chat_api_cjs');
 
 async function chat() {
-  const ctx = await chatapi.initChat(API_KEY);
+  const ctx = chatapi.initChat(API_KEY);
   const result = await chatapi.postChatText("富士山の高さは？", ctx);
   const content = result.content;
   console.log(content);
